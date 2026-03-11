be888072376d6e58e9c1f38c9000ec54c18ad50b
BOTIKTTNET <cgjjjiiookgghhj@gmail.com>
2026-03-11 21:35:01 +0300

    na

--- back/main.py
+++ back/main.py
@@ -1,8 +1,4 @@
 def calculate(f_number, znak, t_number):
-    type_number = type(f_number)
-    typet_number = type(t_number)
-    if type_number or typet_number != int or float:
-        print("Ты чо далбаёб?")
     elif znak != '+' or znak != "-" or znak != "*" or znak != "/":
         result = 'Данный калькулятор не поддерживает такие операции'
         print(result)
@@ -18,5 +14,7 @@ def calculate(f_number, znak, t_number):
     elif znak == "/":
         result = f_number / t_number
         print(result)
-
-calculate()
+if isinstance(f_number, (int, float) and isinstance(t_number, (int, float))):
+    calculate()
+else:
+    print("ZOV")
