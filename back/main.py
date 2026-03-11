e0bd85cc08e99068dc4c05c7c19200d88f4c1f16
BOTIKTTNET <cgjjjiiookgghhj@gmail.com>
2026-03-11 21:15:46 +0300

    na

--- back/main.py
+++ back/main.py
@@ -11,7 +11,9 @@ def calculate(f_number, znak, t_number):
     elif znak == "/":
         result = f_number / t_number
         print(result)
-    elif znak != '+' or znak != "-" or znak != "*" or znak != "/" or f_number != int or float or t_number != int or float:
+    elif znak != '+' or znak != "-" or znak != "*" or znak != "/":
         result = 'Данный калькулятор не поддерживает такие операции'
         print(result)
+    elif f_number or t_number != int or float:
+        print("Ты чо далбаёб?")
 calculate()
