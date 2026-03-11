a135d778a813eb3f4f343116216fe515805ac994
BOTIKTTNET <cgjjjiiookgghhj@gmail.com>
2026-03-11 20:58:07 +0300

    na

---
+++ back/main.py
@@ -0,0 +1,19 @@
+def calculate(f_number, znak, t_number):
+    if znak == "+":
+        result = f_number + t_number
+        print(result)
+    elif znak == "-":
+        result = f_number - t_number
+        print(result)
+    elif znak == "*":
+        result = f_number * t_number
+        print(result)
+    elif znak == "/":
+        result = f_number / t_number
+        print(result)
+    else:
+        result = 'Данный калькулятор не поддерживает такие операции'
+        print(result)
+
+calculate()
+
